45780a70b81ae4a3697eed82a82d211be8e4324a
Lucas Enoki <enoki.lucas@gmail.com>
2023-02-19 21:48:09 -0300

    create playRound()

--- game.js
+++ game.js
@@ -1,10 +1,37 @@
+//Selects a random number between 0, 1, 2 and assigns a play to each number.
 const getComputerChoice = () => {
 	const random = Math.floor(Math.random()*3);
 	if(random === 0) {
-		return 'paper';
+		return 'paper'; //paper
 	} if (random === 1){
-		return 'stone';
+		return 'rock'; //rock
 	} if (random === 2){
-		return 'scissors';
+		return 'scissors'; //scissors
 	}
 }
+
+const playerSelectionA = 'rock'; //Placeholder until a get function is created.
+
+const computerSelection = getComputerChoice();
+const playerSelection = playerSelectionA.trim().toLowerCase();
+
+//Returns the result based on the selections.
+const playRound = (playerSelection, computerSelection) => {
+	if (playerSelection === 'paper' && computerSelection === 'rock') {
+		return 'You Win! Paper beats Rock.';
+	} if (playerSelection === 'paper' && computerSelection === 'scissors') {
+		return 'You Lose! Paper loses to Scissors.';
+	} if (playerSelection === 'rock' && computerSelection === 'paper') {
+		return 'You Lose! Rock loses to Paper.';
+	} if (playerSelection === 'rock' && computerSelection === 'scissors') {
+		return 'You Win! Rock beats Scissors.';
+	} if (playerSelection === 'scissors' && computerSelection === 'paper') {
+		return 'You Win! Scissors beats Paper.';
+	} if (playerSelection === 'scissors' && computerSelection === 'rock') {
+		return 'You Lose! Scissors loses to Rock';
+	} if (playerSelection === computerSelection) {
+		return 'Tie!';
+	}
+}
+
+console.log(computerSelection, playerSelection, playRound(playerSelection, computerSelection));
